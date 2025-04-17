644ac5d0d0df8676170787440ac3933cbe7c1091
urah <74428967+urah001@users.noreply.github.com>
2025-04-17 23:59:14 +0100

    Update App.d24d297a.js

--- _nuxt/App.d24d297a.js
+++ _nuxt/App.d24d297a.js
@@ -5,7 +5,7 @@ import { _ as o, a as s } from "./play-store-purple.6cb5852d.js";
     1
   
         
-        l("div", r, [
+        
           t("div", p, [
             t("span", null, e(a.blok.title), 1),
             t("h3", null, e(a.
