210c780b9129db7fe93a46d0d73d6c99cf82a492
urah <74428967+urah001@users.noreply.github.com>
2025-05-02 03:10:15 +0100

    Update App.d24d297a.js

--- _nuxt/App.d24d297a.js
+++ _nuxt/App.d24d297a.js
@@ -1,2 +1,2 @@
 import { _ as o, a as s } from "./play-store-purple.6cb5852d.js";
-    v class="app-button"><a href="http://onelink.to/ray6xm"><img 
+    ss="app-button"><a href="http://onelink.to/ray6xm"><img 
