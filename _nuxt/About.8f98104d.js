87d9dc89c9c0a8e4e6b3981af7810b4723156031
urah <74428967+urah001@users.noreply.github.com>
2025-05-26 20:47:25 +0100

    Update About.8f98104d.js

--- _nuxt/About.8f98104d.js
+++ _nuxt/About.8f98104d.js
@@ -2,7 +2,7 @@
                             class: "primary-btn hide-mobile",
                           },
                           { default: a(() => [o("Get Started")]), _: 1 }
-                        ),
+                      
                     ]),
                     t("dialog", Q, [
                       S,
