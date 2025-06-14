edb4055b8e37ea1ee340a69d628ed2f3e4f72f64
urah <74428967+urah001@users.noreply.github.com>
2025-06-14 22:50:54 +0100

    Update default.cd887e09.js

--- _nuxt/default.cd887e09.js
+++ _nuxt/default.cd887e09.js
@@ -1,1 +1,1 @@
-import{y as s,o as t,b as o,z as c}from"./entry.c7e512db.js";const r={},a={class:""};function n(e,_){return t(),o("div",a,[c(e.$slots,"default")])}const s(r,[["render",n]]);export{d as default};
+import{y as s,o as t,b as o,z as c}from"./entry.c7e512db.js";const r={},a={class:""};function n(e,_){return t(),o,a,[c(e.$slots,"default")])}const s(r,[["render",n]]);export{d as default};
